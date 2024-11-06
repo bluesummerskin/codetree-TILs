5ad491f777ed74ab55450017a725c03cbfe3648f
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-06 12:21:08 +0900

    Title: 날짜 변경하여 출력 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241106/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int day,month,year;
+    cin>>day
+    cin.get();
+    cin>>month;
+    cin.get();
+    cin>>year;
+
+    cout<<year<<"."<<month<<"."<<day;
+    return 0;
+}
