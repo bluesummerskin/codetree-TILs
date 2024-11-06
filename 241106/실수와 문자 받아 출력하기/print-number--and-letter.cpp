3c63ee17726998bc98e516de2225721255d75fa0
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-06 09:54:33 +0900

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<string>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a;
+    double b,c;
+    cin>>a>>b>>c;
+    cout<<a<<endl;
+    cout<<fixed<<setprecision(2);
+    cout<<b<<endl;
+    cout<<c<<endl;
+    return 0;
+}
