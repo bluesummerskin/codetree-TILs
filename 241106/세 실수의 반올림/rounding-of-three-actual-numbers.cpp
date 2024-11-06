778a0dd9c40d5d6b4b67a7ddda4e8e152d2e1433
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-06 09:50:16 +0900

    Title: 세 실수의 반올림, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout<<fixed<<setprecision(3);
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    return 0;
+}
