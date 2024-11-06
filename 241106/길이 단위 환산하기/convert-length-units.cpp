838c40ced777a9a551bb20888c59560ac3396f30
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-06 09:35:04 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241106/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double fit = 30.48;
+    int a;
+    cin>>a;
+    cout<<fixed<<setprecision(1)<<a*fit;
+    return 0;
+}
