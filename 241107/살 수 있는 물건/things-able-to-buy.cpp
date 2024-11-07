71033b6ab3c995b7bc9e8648dba37fa5ca3cbf87
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-07 13:25:25 +0900

    Title: 살 수 있는 물건, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241107/살 수 있는 물건/things-able-to-buy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int money;
+    cin>>money;
+
+    if(money>=3000){
+        cout<<"Book";
+    }
+    else if(money>=1000){
+        cout<<"mask";
+    }
+    else{
+        cout<<"no";
+    }
+    
+    return 0;
+}
