2dfdeafe1361864b7d7ea7db85b49441f6445d94
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-07 13:36:13 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241107/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int money;
+    cin>>score;
+
+    if(score>=3000){
+        cout<<"book";
+    }
+    else if(score>=1000){
+        cout<<"mask";
+    }
+    else{
+        cout<<"no";
+    }
+    return 0;
+}
