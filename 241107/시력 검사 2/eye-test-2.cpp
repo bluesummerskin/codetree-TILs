ffe3280147185254207fa50bc1fe52a1eef1d8a6
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-07 13:22:29 +0900

    Title: 시력 검사 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241107/시력 검사 2/eye-test-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double sight;
+    cin>>sight;
+
+    if(signt>=1.0){
+        cout<<"High";
+    }
+    else if(sight>=0.5){
+        cout<<"Middle";
+    }
+    else{
+        cout<<"Low";
+    }
+    
+    return 0;
+}
