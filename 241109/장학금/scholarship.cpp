b298a5fff2f4e65da4776525b24947631948fec5
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-09 18:50:19 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241109/장학금/scholarship.cpp
+++ 241109/장학금/scholarship.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int score;
-    cin>>score;
+    int score1;
+    int score2;
+    cin>>score1>>score2;
 
-    if(score>=95){
-        cout<<10;
-    }
-    else if(score>=90){
-        cout<<5;
+    if(score1<90){
+        cout<<0;
     }
     else{
-        cout<<0;
+        if(score2>=95){
+            cout<<10'0000;
+        }
+        else if(score2>=90){
+            cout<<5'0000;
+        }
+        else{
+            cout<<0;
+        }
     }
     return 0;
 }
