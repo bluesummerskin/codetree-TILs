1368a819fa5e7b767e9c3c06a76d1013492d02ed
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-09 18:47:44 +0900

    Title: 장학금, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241109/장학금/scholarship.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int score;
+    cin>>score;
+
+    if(score>=95){
+        cout<<10;
+    }
+    else if(score>=90){
+        cout<<5;
+    }
+    else{
+        cout<<0'
+    }
+    return 0;
+}
