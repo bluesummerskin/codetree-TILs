2cdf1097df63b73e575c094f6712342aa7265d1c
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-10 14:54:45 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
+++ 241110/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a,b;
+    cin>>a>>b;
+    
     if(a<b)
-        cout<<1;
+        cout<<1 << " ";
     else
-        cout<<0;
+        cout<<0 << " ";
 
     if(a==b)
         cout<<1;
     else
         cout<<0;
-        
+
     return 0;
 }
