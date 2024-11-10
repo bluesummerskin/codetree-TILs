956d90136d11d9d02a98031fbf649999e8728b4f
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-10 13:35:16 +0900

    Title: 윤년인가, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241110/윤년인가/is-leap-year.cpp
+++ 241110/윤년인가/is-leap-year.cpp
@@ -6,11 +6,16 @@ int main() {
     int year;
     cin>>year;
 
-    if((year%4!=0) || (year%100==0 && year%400!=0) ){
-        cout<<"false";
+    if(year%4==0){
+        if(year%400!=0 && year%100==0){
+            cout<<"false";
+        }
+        else{
+            cout<<"true";
+        }
     }
     else{
-        cout<<"true";
+        cout<<"false";
     }
     return 0;
 }
