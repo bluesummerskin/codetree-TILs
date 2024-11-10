8ef872f647b70499413b3093d1706dca32eb9e63
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-10 13:15:47 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241110/두 사람/two-person.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n1,n2;
+    string s1,s2;
+    cin>>n1>>s1;
+    cin>>n2>>s2;
+
+    if((n1>=19 || n2>=19) && (s1=="M" || s2=="M")){
+        cout<<1;
+    }
+    else{
+        cout<<0;
+    }
+    return 0;
+}
