fc949bb17518a5e78199cddc62deba50ca1c6399
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-11 16:08:19 +0900

    Title: 높은 수에서 낮은 수까지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241111/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+
+    if(a>b){
+        for(int i=a;i>=b;i++){
+            cout<<i<<" ";
+        }
+    }
+    else if(b>a){
+        for(int i=b;i>=a;i++){
+            cou<<i<<" ";
+        }
+    
+    }
+
+    return 0;
+}
