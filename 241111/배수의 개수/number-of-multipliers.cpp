1ebc39fb6daaed7926d0948ede8bd2be76b1f6fb
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-11 16:19:53 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241111/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt_3 = 0;
+    int cnt_5 = 0;
+
+    for(int i=0;i<10;i++){
+        int n;
+        cin>>n;
+        if(n%3==0){
+            cnt_3++;
+        }
+        if(n%5==0){
+            cnt_5++;
+        }
+    }
+
+    cout<<cnt_3<<" "<<cnt_5;
+    return 0;
+}
