fd0d702bee76ccc4e7295820bd6c2d1dcf88416c
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-11 15:08:45 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/n번 더하기/add-n-times.cpp
+++ 241111/n번 더하기/add-n-times.cpp
@@ -6,12 +6,14 @@ int main() {
     int a,n;
     cin>>a>>n;
 
-    int k=n;
-    cout<<a+n<<endl;
+    int sum = a+n;
+    cout<<sum<<endl;
 
     for(int i=0;i<n-1;i++){
-       n+=k;
-       cout<<a+n<<endl;
+        sum+=n;
+        cout<<sum<<endl;
     }
+
+    
     return 0;
 }
