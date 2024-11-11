5c572a7f1f0f35936b8a53c2a1f70472f3ebfa5f
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-11 19:17:57 +0900

    Title: 5의 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/5의 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 241111/5의 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -7,12 +7,22 @@ int main() {
     int a,b;
     cin>>a>>b;
 
+    if(a<=b){
     for(int i=a;i<=b;i++){
         if(i%5==0){
             sum+=i;
         }
     }
+    }
 
+    else if(a>b){
+        for(int i=b;i<=a;i++){
+            if(i%5==0){
+                sum+=i;
+            }
+        }
+    }
+    
     cout<<sum;
     return 0;
 }
