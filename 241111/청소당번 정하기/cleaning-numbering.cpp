91189fdc517851d90fcd609adf321bf339950daf
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-11 16:40:39 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241111/청소당번 정하기/cleaning-numbering.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt_2 = 0;
+    int cnt_3 = 0;
+    int cnt_12 = 0;
+
+    int n;
+    cin>>n;
+
+    for(int i=1;i<=n-1;i++){
+        
+         if(i%12==0){
+            cnt_12++;
+        }
+
+        if(i%6==0){
+            cnt_3++;
+        }
+        else if(i%2==0){
+            cnt_2++;
+        }
+        else if(i%3==0){
+            cnt_3++;
+        }
+    }
+
+    cout<<cnt_2<<" "<<cnt_3<<" "<<cnt_12;
+
+    return 0;
+}
