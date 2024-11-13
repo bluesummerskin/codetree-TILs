06939c69a9f241add4cbdde901dc9ae841c3cd29
bluesummerskin <173532264+bluesummerskin@users.noreply.github.com>
2024-11-13 09:53:54 +0900

    Title: 0 나올때까지 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241113/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    while(true){
+        int k;
+        cin>>k;
+        if(k==0){
+            break;
+        }
+        cout<<k<<endl;
+    }
+    return 0;
+}
